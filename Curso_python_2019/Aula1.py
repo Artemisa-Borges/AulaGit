e86690d49aabf2873b5f06fede89b488ce5a9ac4
Artemisa-Borges <borges.artemisanc@gmail.com>
2019-07-26 14:49:47 -0300

    some_message2

---
+++ Curso_python_2019/Aula1.py
@@ -0,0 +1,60 @@
+''' print ('olá mundo')
+
+operações matemáticas
+
+# SOMA
+print (2 + 2)
+# Subtração
+print (2 - 2)
+# Divisão
+print (2 / 2)
+# multiplicação
+print (2 * 2)
+# expoente
+print (2 ** 3)
+# retorna o resto de uma divisão
+print (10 % 3)
+
+Variaveis
+var1 = 1 # variável inteira
+var2 = 1.1 # variável float
+var3 = "Eu sou uma string" # variavel string
+var4 = True # variavel boleana
+var4 = False # variáveç boleana
+
+print (var1)
+print (var2)
+print (var3)
+print (var4)
+
+operadores
+x = 2
+y = 3
+
+print (x==y)
+
+operadores condicionais'''
+x = 3
+y = 7
+
+if x > y:
+	print ("x é maior que y")
+else:
+	print ("x não é maior que y")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
